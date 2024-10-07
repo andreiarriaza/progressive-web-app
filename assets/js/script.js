abdb9d8cca296a2547626552d7a8dda020e6c34d
IT-Plastikito <it.plastikito@gmail.com>
2024-10-07 10:19:00 -0600

    Crear ServiceWorker

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
 /* Un ServiceWorker es un archivo JavaScript que funciona en segundo plano. */
 
-if ("ServiceWorker" in Navigator) {
+if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register("./sw.js")
+    /* Acá se debe indicar la ubicación del archivo "sw.js" tomando como referencia la ubicación del archivo "index.html". */
+    .register("./assets/js/sw.js")
     .then((reg) => console.log("Registro de SW (Service Worker) exitoso", reg))
     .catch((err) =>
-      console.error("Error al tratar de registrar el SW (ServiceWorker)")
+      console.error("Error al tratar de registrar el SW (ServiceWorker)", err)
     );
 }
