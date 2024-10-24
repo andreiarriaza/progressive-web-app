4299e89687d1cbe15d26f7332535b3448e3c0ba2
IT-Plastikito <it.plastikito@gmail.com>
2024-10-24 09:50:26 -0600

    Actualizar

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,23 @@
 
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    /* Acá se debe indicar la ubicación del archivo "sw.js" tomando como referencia la ubicación del archivo "index.html". */
+    /* Acá se debe indicar la ubicación del archivo "sw.js" tomando como referencia la ubicación del archivo "index.html". 
+    
+    Debido a que este archivo será importado en todas las páginas, es necesario definir una ruta absoluta para indicar la ubicación del ServiceWorker. 
+
+    La "/" hace referencia a que buscará en la carpeta raíz, sin embargo, como en este caso la página se subirá a GithubPages, esa ruta raíz es:      
+              andreiarriaza.github.io 
+
+    Por lo que para acceder al ServiceWorker es necesario especificar además el nombre del repositorio dentro del cual se encuentra; dicho respositorio es "progressive-web-app", por eso quedó de la siguiente manera: 
+
+        andreiarriaza.github.io/progressive-web-app/assets/js/sw.js
+
+    
+    Si solamente se fuera a probar localmente la Progressive Web App, o bien, si esta progressive web app se fuera a subir directamente a la carpeta "Public HTML" de determinado hosting, no haría falta incluir el nombre del repositorio obviamente, porque la ruta raíz directamente apuntaría a la carpeta "public HTML", quedando de la siguiente manera: 
+
+              /assets/js/sw.js"
+
+    */
     .register("/progressive-web-app/assets/js/sw.js")
     .then((reg) => console.log("Registro de SW (Service Worker) exitoso", reg))
     .catch((err) =>
