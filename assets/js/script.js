616c01c988be43f9156d6c79bc569ad4c1d244e5
IT-Plastikito <it.plastikito@gmail.com>
2024-10-24 08:18:55 -0600

    ACtualizar URL

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
     /* Acá se debe indicar la ubicación del archivo "sw.js" tomando como referencia la ubicación del archivo "index.html". */
-    .register("/assets/js/sw.js")
+    .register("assets/js/sw.js")
     .then((reg) => console.log("Registro de SW (Service Worker) exitoso", reg))
     .catch((err) =>
       console.error("Error al tratar de registrar el SW (ServiceWorker)", err)
