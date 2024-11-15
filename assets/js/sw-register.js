bab9dbbe89c9c00e814258deeddd1b72110c5dfb
IT-Plastikito <it.plastikito@gmail.com>
2024-11-15 15:38:10 -0600

    Acutalizar

--- assets/js/sw-register.js
+++ assets/js/sw-register.js
@@ -3,7 +3,7 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("./sw.js")
+      .register("/progressive-web-app/assets/js/sw.js")
       .then((registration) => {
         console.log("Service Worker registrado con éxito:", registration);
       })
